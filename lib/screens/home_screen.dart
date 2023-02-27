d48731cb263989524beb4aed09edcdb6e656a45a
FaisalAli8285 <faisalali159876@gmail.com>
2023-02-27 05:36:32 -0800

    First Comit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+
+import 'package:apiss/model/post_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postlist = [];
+  Future<List<PostModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map<String, dynamic> i in data) {
+        postlist.add(PostModel.fromJson(i));
+      }
+      return postlist;
+    } else {
+      return postlist;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("API COURSE")),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Text("loading");
+                } else {
+                  return ListView.builder(
+                    itemCount: postlist.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        //color: Colors.green,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("TITLE",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                              Text(postlist[index].title.toString()),
+                              Text("DEDCRIPTION",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                              Text(postlist[index].body.toString()),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
